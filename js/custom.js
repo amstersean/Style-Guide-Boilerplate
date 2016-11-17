973172257c962e3f6bed41006f673a6f80dd195b
amstersean <samster@frontlinetechnologies.com>
2016-11-17 09:07:11 -0500

    added Copied feedback

--- js/custom.js
+++ js/custom.js
@@ -12,4 +12,23 @@ $(document).ready(function () {
             return copiedColor;
         }
     });
+
+    clipboard.on('success', function(e) {
+
+        var $clickedDiv = $(e.trigger);
+        var $spanColor = $clickedDiv.find(".sg-animated");
+        var originalColor = $spanColor.text();
+        $spanColor.text("Copied!");
+        setTimeout(function () { 
+            $spanColor.text(originalColor);
+         } , 2500);
+
+        e.clearSelection();
+    });
+
+    clipboard.on('error', function(e) {
+        console.error('Action:', e.action);
+        console.error('Trigger:', e.trigger);
+        console.error('programatic copying to your clipboard is not available :(');
+    });
 });
